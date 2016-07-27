929d1beaaf65223012eaf5532791102b4709ee2e
DidJS <didier.cauvin@outlook.fr>
2016-07-27 16:52:06 +0200

    refactoring

--- lib/directorytree.ex
+++ lib/directorytree.ex
@@ -26,31 +26,23 @@ defmodule Directorytree do
             IO.puts build_line(depth) <> "#{file}"
             if File.dir?(fullname), do: acc = lstat_recursive(fullname, acc, depth + 1)
 
-            update(File.dir?(fullname), acc)
+            {_old, map} =
+                if File.dir?(fullname), do: get_and_update(acc, :d), else: get_and_update(acc, :f)
+
+            map
         end)
-    end
-
-    defp update(isdir, map) when isdir do
-        {_old, map} = get_and_update(map, :d)
-        map
-    end
-
-    defp update(_isdir, map) do
-        {_old, map} = get_and_update(map, :f)
-        map
     end
 
     defp get_and_update(map, prop) do
         Map.get_and_update(map, prop, fn n -> {n, n + 1} end)
     end
 
-    defp build_line(depth) when depth == 1 do
-        "|__ "
-    end
+    defp build_line(depth, line \\ "")
 
-    defp build_line(depth) do
-        "    " <> build_line(depth - 1)
-    end
+    defp build_line(depth, line) when depth == 1, do: line <> "|__ "
+
+    defp build_line(depth, line), do: build_line(depth - 1, line <> "    ") # Tail recursive
+
 
 
 end
